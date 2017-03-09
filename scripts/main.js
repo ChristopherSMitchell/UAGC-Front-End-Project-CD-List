bca6a00d9d5177821df80798cc70418693afe2b5
MrMPerial <chris@csmitchell.com>
2017-03-09 15:44:04 -0600

    Added Handlebars

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,73 @@
-$(document).ready(function () {
+// Production
+var theRequest = new XMLHttpRequest();
+theRequest.open('GET', 'main.json');
+theRequest.onload = function () {
+    if (theRequest.status >= 200 && theRequest.status < 400) {
+        var data = JSON.parse(theRequest.responseText);
+        createHTML(data);
+    } else {
+        console.log("Oops, Something went wrong!");
+    }
+};
 
-    // Better Dayz
-    $('#betterDayz').hide();
-    
-    $('#bdOpen').on('click', function () {
-        $('#betterDayz').fadeIn(500);
-    });
+theRequest.onerror = function() {
+    console.log("Connection error");
+};
 
-    $('#bdClose').on('click', function () {
-        $('#betterDayz').fadeOut(500);
-    });
-    
-    // Pacs Life
-    $('#pacsLife').hide();
-    
-    $('#plOpen').on('click', function () {
-        $('#pacsLife').fadeIn(500);
-    });
+theRequest.send();
 
-    $('#plClose').on('click', function () {
-        $('#pacsLife').fadeOut(500);
-    });
+function createHTML(albumsData) {
+    var rawTemplate = document.getElementById('albumsTemplate').innerHTML;
+    var compiledTemplate = Handlebars.compile(rawTemplate);
+    var theGeneratedHTML = compiledTemplate(albumsData);
     
+    var albumsContainer = document.getElementById('albums-container');
+    albumsContainer.innerHTML = theGeneratedHTML;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Testing
+/*var pageCounter = 1;
+var artistContainer = document.getElementById('artist-Info');
+var btn = document.getElementById('btn');
+
+btn.addEventListener('click', function () {
+    var theRequest = new XMLHttpRequest();
+    theRequest.open('GET', 'main-' + pageCounter + '.json');
+    theRequest.onload = function () {
+        var theData = JSON.parse(theRequest.responseText);
+        renderHTML(theData);
+        pageCounter++;
+        
+        if(pageCounter > 5) {
+            btn.classList.add('hide');
+        }
+    };
+
+    theRequest.send();
 });
+
+function renderHTML(data) {
+    htmlString = "";
+    
+    for(i = 0; i < data.albums.length; i++) {
+        htmlString += "<p style=\"background-color:gray;padding:20px;font-size:32px;color:black\">" + data.albums[i].artist + " is an Artist in the " + data.albums[i].genre + " genre.</p>"
+    }
+    
+    artistContainer.insertAdjacentHTML('beforeend', htmlString);
+}*/
