872c7687e45d2db3261074ed704e24bf74992b7f
MrMPerial <chris@csmitchell.com>
2017-04-04 11:18:32 -0500

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ theRequest.onload = function () {
     }
 };
 
-theRequest.onerror = function() {
+theRequest.onerror = function () {
     console.log("Connection error");
 };
 
@@ -19,7 +19,52 @@ function createHTML(albumsData) {
     var rawTemplate = document.getElementById('albumsTemplate').innerHTML;
     var compiledTemplate = Handlebars.compile(rawTemplate);
     var theGeneratedHTML = compiledTemplate(albumsData);
-    
+
     var albumsContainer = document.getElementById('albums-container');
     albumsContainer.innerHTML = theGeneratedHTML;
+
+    $('.btn').on('click', openModal);
+    $('.close').on('click', closeModal);
+
+    function openModal() {
+        /*var target = $(this).data('target');
+        $(target).fadeIn(100, function () {
+            $(this).find('.modal').fadeIn(100);
+        });
+        console.log(target);*/
+        var artistId = $(this).data('id');
+        var album = searchAlbums(artistId);
+        console.log(album);
+
+        var rawAlbumTemplate = document.getElementById('modalTemplate').innerHTML;
+        var compiledAlbumTemplate = Handlebars.compile(rawAlbumTemplate);
+        var theGeneratedAlbumHTML = compiledAlbumTemplate(album);
+
+        var albumContainer = document.getElementById('modalContent');
+        albumContainer.innerHTML = theGeneratedAlbumHTML;
+        
+        $('.modal').fadeIn();
+    }
+
+    function searchAlbums(artistName) {
+        var albums = albumsData.albums;
+        for (i = 0; i < albums.length; i++) {
+            console.log(albums[i].artist);
+            console.log(artistName);
+            if (albums[i].artist == artistName) {
+                return albums[i];
+            }
+        }
+    }
+
+    function closeModal() {
+        $('.modal').fadeOut(100, function () {
+            $('.overlay').fadeOut(100);
+        });
+
+        $(document).off('keyup', function () {
+            $('.overlay').off('click', closeModal);
+        });
+    }
+
 };
